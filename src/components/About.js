b603005079462e7025b341d4d2b760d1a51dec3c
Adarsh <adarshgupta2468@gmail.com>
2021-03-20 13:56:22 +0530

    responsiveness

--- src/components/About.js
+++ src/components/About.js
@@ -6,6 +6,8 @@ const About = () => {
     <div className="about">
       {" "}
       <div className="about__top"></div>
+      <div className="about__left"></div>
+      <div className="about__bottom"></div>
       <div className="top">
         <h1>I </h1> <h1>Design </h1> <h1>And </h1>
         <h1>Build </h1>
@@ -47,21 +49,21 @@ const About = () => {
           You can also call me Frontend-Developer,Freelancer,Music lover,Space
           lover,Animal lover,advisor
         </h3>
-        <p>
-          I want to define myself by the work I want to do.Skill can be
-          developed, Character can be build. I love to learn new stuffs and also
-          loves to teach
+        <p className="P_text">
+          I want to define myself by the work I love to do.Skill's can be
+          developed, Character can be build & not everything is easy . I love to
+          learn new stuffs and also loves to teach
         </p>
-        <p>
-          I believe Humanity will end in other planet and not in our
+        <p className="P_text">
+          I believe Humanity will end in another planet and not in our
           Earth.Coding and Space are my favourite things.{" "}
         </p>
-        <p>
+        <p className="P_text">
           The First step is always the difficult one and it took one year to
           understand that coding is just pattern recognising and not learning
           each and every syntax
         </p>
-        <p>Atlast I guess Iam an selectively introvert </p>
+        <p className="P_text">Alas, I guess Iam an selectively introvert </p>
       </div>
     </div>
   );
